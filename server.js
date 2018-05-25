859efb2cd75360b268308b13f22c9839a50ab079
200360407 <200360407.student.georgianc.on.ca>
2018-05-25 11:27:09 -0400

    Hello Everyone

---
+++ server.js
@@ -0,0 +1,25 @@
+var connect = require ( 'connect');
+var random_insult = require( './random_insult' );
+var app = connect();
+
+var helloWorld = function( req, res, next ){
+  res.setHeader( 'Content-Type', 'text-plain');
+
+  res.end( 'Hello World');
+};
+
+var insultMe = function (req, res, next ){
+  res.setHeader( 'Content-Type', 'text-plain');
+
+random_insult.get_insult( function (insult){
+res.end( insult );
+
+  });
+};
+
+app.use( '/hello', helloWorld);
+app.use( '/app_me', insultMe );
+
+app.listen( 3001);
+
+console.log( 'Server running at http://localhost:3001' );
